08668a87a469f63630008385c354b79b7cd2569e
rodrimoni <rodrimoni@gmail.com>
2019-03-29 14:01:55 -0300

    Add first files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+/* CivisAnalysis Update Server */
+
+var express = require('express');
+var camara = require('./routes/camara');
+//var camaraClient = require('./routes/camaraClient');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var db;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@civisanalysisdb-rgysv.mongodb.net/test?retryWrites=true";
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+	if (err) return console.log(err);  
+	db = client.db("CivisAnalysis");
+	
+	// functions to update DB... HTTP GET from camara.gov --------------------------------------
+	app.get('/obterDeputados', camara.obterDeputados(db));
+	app.get('/listarTodasProposicoesVotadasEmPlenario', camara.listarTodasProposicoesVotadasEmPlenario(db));
+	app.get('/obterVotacaoProposicao/:tipo/:numero/:ano', camara.obterVotacaoProposicao(db));
+	app.get('/obterProposicao/:tipo/:numero/:ano', camara.obterProposicao(db));
+	
+	// CREATE SERVER :3000
+  	http.createServer(app).listen(app.get('port'), function(){
+		console.log('Express server listening on port ' + app.get('port'));
+	});
+});
+
+app.use(express.json({limit: '10mb'}));
+
+app.route('/') //setado a rota, e abaixo as ações a serem tomadas dentro desta rota
+	.get(function(req, res) {
+		res.render('index.ejs')
+	})
---
+++ routes/camara.js
@@ -0,0 +1,174 @@
+
+/*
+* GET camara resources and save to DB
+* NOTE: this functions will make a GET to camara.gov and update the mongo db.
+*/
+
+// XML -> JSON parser -----------------------
+var xml2js     = require('xml2js');
+xml2js.defaults['0.2'].explicitArray = false;
+xml2js.defaults['0.2'].mergeAttrs    = true;
+// ------------------------------------------
+
+var request = require('request');
+
+exports.obterDeputados = function(db){ 
+return function(req, res){
+	request.post('https://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados?', 
+	{}, 
+	(err, result, body) => {
+			if (err) {
+				console.error(error)
+				return
+			}
+			console.log(`statusCode: ${result.statusCode}`)
+			xml2js.parseString(body, function(err,json){ 
+				if (err) {
+					console.error(error)
+					return
+				}
+				db.collection('obterDeputados').insertOne(json, function(err, result){
+					res.json(
+						(err === null) ? json : { msg: err.message }
+					);
+				});
+			})
+		})
+	};	
+};
+
+
+// Get the list of all 'articles' voted in plenary (representatives chamber = camara dos deputados)
+exports.listarProposicoesVotadasEmPlenario = function(db){
+return function(req, res){
+	var ano = req.params.ano; // get the list of roll calls in the year(==ano)
+	// GET from camara the response
+	request.get('https://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoesVotadasEmPlenario?ano='+ano+'&tipo=', 
+	{}, 
+	(err, result, body) => {
+			if (err) {
+				console.error(error)
+				return
+			}
+			console.log(`statusCode: ${result.statusCode}`)
+			xml2js.parseString(body, function(err,json){ 
+				if (err) {
+					console.error(error)
+					return
+				}
+				db.collection('listarProposicoesVotadasEmPlenario').update({ano:ano},{ano:ano,data:json},{upsert:true}, function(err, result){
+				res.json(
+                  (err === null) ? {ano:ano, data:json} : { msg: err }
+                );
+              });
+			})
+		})
+	};  
+};
+
+//
+// INSERT in the new entries => datetime = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+//http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo=PL&numero=1992&ano=2007
+//
+exports.obterVotacaoProposicao = function(db){
+return function(req, res){
+	var ano = req.params.ano;
+	var tipo = req.params.tipo;
+	var numero = req.params.numero;
+	
+	request.get('http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo='+tipo+'&numero='+numero+'&ano='+ano,
+	{}, (err, result, body) => { 
+			if (err) {
+				console.error(error)
+				return
+			}
+			console.log(`statusCode: ${result.statusCode}`)
+
+			xml2js.parseString(body, function(err,json){ 
+				if (err) {
+					console.error(error)
+					return
+				}
+
+				// fix and add variables
+				json = fixFormatObterVotacaoProposicao(json);
+
+				// add the datetimeRollCallsMotion entry reference to the motion 
+				for (var i = 0; i < json.proposicao.Votacoes.Votacao.length; i++) {
+					db.collection('datetimeRollCallsMotion')
+					.update({'datetime':json.proposicao.Votacoes.Votacao[i].datetime,'tipo':tipo,'numero':numero,'ano':ano}, //query
+							{'datetime':json.proposicao.Votacoes.Votacao[i].datetime,'tipo':tipo,'numero':numero,'ano':ano}, //insert/update
+							{upsert:true},                                                                                   // param
+							function(err, result){ if(err != null){console.log(err)} }                                      // callback
+					); 
+					
+				};           
+
+				// add to the collection of motionRollCalls and return the json;
+				db.collection('obterVotacaoProposicao')
+					.update({'proposicao.Sigla':tipo,'proposicao.Numero':numero,'proposicao.Ano':ano},      //query
+							json,                                                                          //insert/update
+							{upsert:true},                                                                 // param
+							function(err, result){  res.json(  (err === null) ? json : { msg: err }  )  }  // callback
+					); 
+		})          
+	}) // requestify
+};  
+};
+  
+exports.obterProposicao = function(db){
+return function(req, res){
+	var ano = req.params.ano;
+	var tipo = req.params.tipo;
+	var numero = req.params.numero;
+	request.get('http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterProposicao?tipo='+tipo+'&numero='+numero+'&ano='+ano,
+	{}, 
+	(err, result, body) => { 
+		if (err) {
+			console.error(error)
+			return
+		}
+		console.log(`statusCode: ${result.statusCode}`)	
+		xml2js.parseString(body, function(err,json){ 
+				if (err) {
+					console.error(error)
+					return
+				}
+				//FIX the proposicao.tipo => sometimes with whitespaces++
+				//console.log(json.proposicao.tipo);
+				json.proposicao.tipo = json.proposicao.tipo.trim();
+
+				db.collection('obterProposicao')
+					.update({'proposicao.tipo':tipo,'proposicao.numero':numero,'proposicao.ano':ano}, json,{upsert:true}, function(err, result){
+					res.json(
+						(err === null) ? json : { msg: err }
+					);
+				});
+		})
+	});
+	};  
+};
+
+function fixFormatObterVotacaoProposicao(json){
+	//FIX the proposicao.tipo => sometimes with whitespaces++
+	json.proposicao.Sigla = json.proposicao.Sigla.trim(); 
+  
+	// fix the object/array to array
+	if(!isArray(json.proposicao.Votacoes.Votacao)){
+	  json.proposicao.Votacoes.Votacao = [ json.proposicao.Votacoes.Votacao ];
+	}
+  
+	// ADD datetime Date()
+	for (var i = 0; i < json.proposicao.Votacoes.Votacao.length; i++) {
+	  var day_month_year = json.proposicao.Votacoes.Votacao[i].Data.match(/\d+/g);
+	  var hour_minutes = json.proposicao.Votacoes.Votacao[i].Hora.match(/\d+/g);
+	  json.proposicao.Votacoes.Votacao[i].datetime = 
+		new Date(day_month_year[2], day_month_year[1]-1, day_month_year[0], hour_minutes[0], hour_minutes[1], 0, 0);
+	};
+	return json;
+}     
+
+// check is the Object is an Arrayroposicoes2012
+function isArray(obj) {
+	return Object.prototype.toString.call(obj) === '[object Array]';
+}
